13d76e91f65af96a2869b1a32448a03befd2ecb4
chikaltyaga <tyaga.chikal@gmail.com>
2019-11-22 16:20:06 +0700

    final

---
+++ LATIHAN_1/src/latihan_1/LATIHAN_1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_1;
+
+/**
+ *
+ * @author Asus X 441B
+ */
+public class LATIHAN_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------") ;
+    System.out.println("NAMA\t\t: Chikal Tyaga");
+    System.out.println("ALAMAT\t\t: Dsn Cinungku RT 01/RW 04");
+    System.out.println("TEMPAT LAHIR\t: Jakarta");
+    System.out.println("TANGGAL LAHIR\t: 04 Agustus 1999");
+    System.out.println("---------------------");
+    System.out.println("HOBI\t\t: Mencari Uang");
+    System.out.println("MAKES\t\t: Richesse");
+    System.out.println("MIKES\t\t: Thai Tea");
+    }
+}
